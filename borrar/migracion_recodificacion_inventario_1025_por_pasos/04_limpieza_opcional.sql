8a006188b117996c16d1ff94f93fdc4a935f126b
Roberto <roberto.bergua@gmail.com>
2026-06-25 16:39:20 +0200

    1

--- borrar/migracion_recodificacion_inventario_1025_por_pasos/04_limpieza_opcional.sql
+++
@@ -1,3 +0,0 @@
--- PASO 4 OPCIONAL: limpieza de tabla auxiliar tras validar que todo está correcto.
--- Ejecutar solo cuando hayas comprobado la app y las validaciones finales.
-drop table if exists public.tmp_recodificacion_inventario;
